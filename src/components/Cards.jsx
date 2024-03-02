1749ffbbec145a2b5567ef051557f9f157e29052
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-03-02 19:11:57 +0530

    Feature

---
+++ src/components/Cards.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='w-full h-screen flex items-center px-32 gap-5'>
+        <div className='cardcontainer h-[50vh] w-1/2'>
+            <div className='card relative rounded-xl w-full h-full flex items-center justify-center bg-[#004d43]'>
+                <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg" alt="" />
+                <button className='absolute px-5 py-1 rounded-full border-[1px] left-10 border-[#CDEA68] bottom-10 text-[#CDEA68]'>&copy;2019-2022</button>
+            </div>
+        </div>
+        <div className='cardcontainer flex gap-5 h-[50vh] w-1/2'>
+          <div className='card relative flex items-center justify-center rounded-xl w-1/2 h-full bg-zinc-800'>
+          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg" alt="" />
+                <button className='absolute px-5 py-1 rounded-full border-[1px] left-10 border-white uppercase tracking-tighter bottom-10 text-zinc-100'>Rating 5.0 on clutch</button>
+          </div>
+          <div className='card relative flex items-center justify-center rounded-xl w-1/2 h-full bg-zinc-800'>
+          <img className='w-32' src="https://ochi.design/wp-content/uploads/2022/04/logo003.png" alt="" />
+                <button className='absolute px-5 py-1 rounded-full border-[1px] left-10 border-white bottom-10 uppercase tracking-tighter text-zinc-100'>business bootcamp alumni</button>
+          </div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Cards
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -9,13 +9,21 @@ const Feature = () => {
         </div>
         <div className='px-20'>
         <div className='cards w-full flex gap-10 mt-10'>
-                <div className='cardcontainer    w-1/2 h-[75vh]  '>
+             
+                <div className='cardcontainer  relative w-1/2 h-[75vh]  '>
+                  <h1 className='absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] z-[9] text-8xl  leading-none tracking-tighter font-teko'>
+                     {"FYDE".split('').map((item,index)=><span>{item}</span>)}
+                     </h1>
                     <div className='card w-full h-full rounded-xl overflow-hidden '> 
                         <img className='w-full h-full bg-cover ' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
                     </div>
                 </div>
 
-                <div className='cardcontainer    w-1/2 h-[75vh]   '>
+                <div className='cardcontainer relative   w-1/2 h-[75vh]   '>
+                <h1 className='absolute right-full translate-x-1/2 top-1/2 translate-y-1/2 text-[#CDEA68] z-[9] text-8xl  leading-none tracking-tighter font-teko'>
+                  {"VISE".split('').map((item,index)=><span>{item}</span>)}
+                </h1>
+
                     <div className='card w-full h-full rounded-xl overflow-hidden'>
                         <img className='w-full h-full bg-cover '   src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="" />
                     </div>
